2581011a1199e1a62cc1829a6e3667e413670479
Ivo <ivokalendar@icloud.com>
2020-03-25 17:28:18 +0100

    setState of Lantitude and Longitude...

--- src/frontend/react/components/index.jsx
+++ src/frontend/react/components/index.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import SeasonDisplay from './seasonDisplay.jsx'
 
 class Index extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = { lat: null, long: null, errMsg: '' }
+
+		window.navigator.geolocation.getCurrentPosition(
+			position => {
+				this.setState({ 
+					lat: position.coords.latitude, 
+					long: position.coords.longitude 
+				})
+			},
+			err => {
+				this.setState({ errMsg: err.message })
+			}
+		)
+	}
+
 	render() {
-		return <h1>Hello from Index</h1>
+		return (
+			<div className="test">
+				<SeasonDisplay />
+				{
+					!this.state.lat && !this.state.errMsg
+					? 	<h1 className="green alert">
+							Loading<span className="red">!!!</span>
+						</h1>
+					: 	this.state.errMsg && !this.state.lat
+					? 	<h1 className="red alert">Error: {this.state.errMsg}</h1>
+					: 	<div className="green alert">
+							<h1>Latitude: {this.state.lat}</h1>
+							<h1>Longitude: {this.state.long}</h1>
+						</div>
+				}
+			</div>
+		)
+
 	}
 }
 
---
+++ src/frontend/react/components/seasonDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class SeasonDisplay extends React.Component {
+	render() {
+		return (
+			<h1>SeasonDisplay!!!</h1>
+		)
+	}
+}
+
+export default SeasonDisplay
