34649489f19c956fce8a644e9d8fbee8e2e66518
Ivo <ivokalendar@icloud.com>
2020-03-26 01:08:53 +0100

    Completed My Little Project...

--- src/frontend/react/components/index.jsx
+++ src/frontend/react/components/index.jsx
@@ -1,44 +1,36 @@
 import React from 'react'
 import SeasonDisplay from './seasonDisplay.jsx'
+import Spiner from './spiner.jsx'
 
 class Index extends React.Component {
 	constructor(props) {
 		super(props)
+		this.state = { lat: null, errMsg: '' }
+	}
 
-		this.state = { lat: null, long: null, errMsg: '' }
-
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
 			position => {
-				this.setState({ 
-					lat: position.coords.latitude, 
-					long: position.coords.longitude 
-				})
+				this.setState({ lat: position.coords.latitude })
 			},
-			err => {
-				this.setState({ errMsg: err.message })
-			}
+			err => this.setState({ errMsg: err.message })
 		)
 	}
 
 	render() {
-		return (
-			<div className="test">
-				<SeasonDisplay />
-				{
-					!this.state.lat && !this.state.errMsg
-					? 	<h1 className="green alert">
-							Loading<span className="red">!!!</span>
-						</h1>
-					: 	this.state.errMsg && !this.state.lat
-					? 	<h1 className="red alert">Error: {this.state.errMsg}</h1>
-					: 	<div className="green alert">
-							<h1>Latitude: {this.state.lat}</h1>
-							<h1>Longitude: {this.state.long}</h1>
-						</div>
-				}
-			</div>
-		)
+		if (!this.state.lat && !this.state.errMsg) {
+			return <Spiner message="Ве молиме дозволете пристап до вашата Локација!" />
+		} else if (this.state.errMsg && !this.state.lat) {
+			return (
+				<div className="container">
+					<h1 className="red alert">Error: {this.state.errMsg}</h1>
+				</div>
+			)
+		} 
 
+		return (
+			<SeasonDisplay lat={this.state.lat} />
+		)
 	}
 }
 
--- src/frontend/react/components/seasonDisplay.jsx
+++ src/frontend/react/components/seasonDisplay.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
-class SeasonDisplay extends React.Component {
-	render() {
-		return (
-			<h1>SeasonDisplay!!!</h1>
-		)
-	}
+const seasonConfig = {
+	winter: { result: 'Брр Надвор е Ладно.', iconName: 'snowflake' },
+	summer: { result: 'Идеме на Плажа!', iconName: 'sun' }
+}
+
+const getSeason = (lat, month) => {
+	if (month > 2 && month < 9) return lat > 0 ? 'summer' : 'winter' 
+	return lat > 0 ? 'winter' : 'summer'
+}
+
+const SeasonDisplay = props => {
+	const season = getSeason(props.lat, new Date().getMonth())
+	const { result, iconName } = seasonConfig[season]
+
+	return (
+		<div className={`container alert ${season}`}>
+			<i className={`icon-left huge ${iconName} icon`} />
+			<h1 className="result">{result}</h1>
+			<i className={`icon-right massive ${iconName} icon`} />
+		</div>
+	)
 }
 
 export default SeasonDisplay
